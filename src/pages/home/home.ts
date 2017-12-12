51c4b398fde409e3bfc6fb06d99c7f597db3813c
Staff <staff@MacBook-Pro-de-Xcode.local>
2017-12-12 15:18:20 +0100

    Added style and edge button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,19 +4,15 @@ import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+    providers: [[YoutubeVideoPlayer]]
 })
 export class HomePage {
 
   constructor(public navCtrl: NavController ,private youtube: YoutubeVideoPlayer) {
 
   }
-  private playVideo(id){
-    try{
-      this.youtube.openVideo(this.$id);
-    }
-    catch(e){
-      console.error(e);
-    }
+  private playVideo(){
+      this.youtube.openVideo("v=2bjk26RwjyU");
   }
 }
