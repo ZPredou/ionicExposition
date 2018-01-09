6afb7455f3f3f2c194818e7c077b9e510b4cd2a0
Decourty William <williamdecourty@live.fr>
2018-01-09 18:00:59 +0100

    Ajout fonctionalitée en savoir plus sur details

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -13,12 +13,14 @@ export class DetailPage {
   description:string;
   image:string;
   author:string;
+  url:string;
   constructor(public nav: NavController, public navParams: NavParams, private vibration: Vibration) {
-    this.navParams = navParams;
-    this.title = this.navParams.get('contact').title;
+    this.navParams   = navParams;
+    this.title       = this.navParams.get('contact').title;
     this.description = this.navParams.get('contact').description;
-    this.image = this.navParams.get('contact').image;
-    this.author = this.navParams.get('contact').author;
+    this.image       = this.navParams.get('contact').image;
+    this.author      = this.navParams.get('contact').author;
+    this.url         = this.navParams.get('contact').url;
   }
   private vibrate(){
     this.vibration.vibrate([30]);
