8d5f9aaa26faeafb793e1a4e33e73e8286d997ae
Decourty William <williamdecourty@live.fr>
2018-01-22 10:44:00 +0100

    Ionic CDDC

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -23,7 +23,7 @@ export class AboutPage {
        "title"       : "AMERICA THE BEAUTIFUL",
        "image"       : "assets/imgs/americathebeautiful.jpg",
        "description" : "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.",
-       "url"         : "http://www.fondationfrances.com/artistes/jonathan-callan/",
+       "url"         : "http://www.fondationfrances.com/artistes/jonathan-callan/#245",
        "sound"       : "assets/sounds/School-bell-sound-effect.mp3"
       },
       {"id"          : 1,
